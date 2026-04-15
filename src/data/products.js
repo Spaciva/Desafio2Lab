823a535d7faf807091388735aaaa72bdf383109d
PacoTracer <fa098138@gmail.com>
2026-04-15 15:59:00 -0600

    fix: correct image URL paths

--- src/data/products.js
+++ src/data/products.js
@@ -1,12 +1,12 @@
 export const products = [
-  { id: '1', name: 'Tacos al Pastor', price: 3.50, image: require('../assets/tacos.jpg'), type: 'food' },
-  { id: '2', name: 'Enchiladas', price: 4.00, image: require('../assets/enchiladas.jpg'), type: 'food' },
-  { id: '3', name: 'Agua de Horchata', price: 1.50, image: require('../assets/horchata.jpg'), type: 'drink' },
-  { id: '4', name: 'Coca Cola', price: 2.00, image: require('../assets/coca.jpg'), type: 'drink' },
-  { id: '5', name: 'Agua Mineral', price: 1.00, image: require('../assets/agua.jpg'), type: 'drink' },
-  { id: '6', name: 'Guacamole', price: 3.00, image: require('../assets/guacamole.jpeg'), type: 'food' },
-  { id: '7', name: 'Quesadillas', price: 3.50, image: require('../assets/quesadillas.jpg'), type: 'food' },
-  { id: '8', name: 'Chiles Rellenos', price: 4.50, image: require('../assets/chiles.jpg'), type: 'food' },
-  { id: '9', name: 'Sopes', price: 2.50, image: require('../assets/sopes.jpg'), type: 'food' },
-  { id: '10', name: 'Tamales', price: 2.00, image: require('../assets/tamales.jpg'), type: 'food' }
+  { id: '1', name: 'Tacos al Pastor', price: 3.50, image: require('../../assets/tacos.jpg'), type: 'food' },
+  { id: '2', name: 'Enchiladas', price: 4.00, image: require('../../assets/enchiladas.jpg'), type: 'food' },
+  { id: '3', name: 'Agua de Horchata', price: 1.50, image: require('../../assets/horchata.jpg'), type: 'drink' },
+  { id: '4', name: 'Coca Cola', price: 2.00, image: require('../../assets/coca.jpg'), type: 'drink' },
+  { id: '5', name: 'Agua Mineral', price: 1.00, image: require('../../assets/agua.jpg'), type: 'drink' },
+  { id: '6', name: 'Guacamole', price: 3.00, image: require('../../assets/guacamole.jpeg'), type: 'food' },
+  { id: '7', name: 'Quesadillas', price: 3.50, image: require('../../assets/quesadillas.jpg'), type: 'food' },
+  { id: '8', name: 'Chiles Rellenos', price: 4.50, image: require('../../assets/chiles.jpg'), type: 'food' },
+  { id: '9', name: 'Sopes', price: 2.50, image: require('../../assets/sopes.jpg'), type: 'food' },
+  { id: '10', name: 'Tamales', price: 2.00, image: require('../../assets/tamales.jpg'), type: 'food' }
 ];
